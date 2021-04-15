d4b4b3f8c8e58e1cf7024648b688eb1956a80a73
datakitsch9761 <josephhwanlee9761@gmail.com>
2021-04-15 23:56:16 +0900

    day02 - Udemy Lecture practice

--- src/iostream/ReadInputs.java
+++ src/iostream/ReadInputs.java
@@ -1,32 +1,48 @@
 package iostream;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class ReadInputs {
     public static void main(String[] args) throws IOException {
-        File file = new File("src/Test.txt");
+        File file = new File("src/iostream/Test.txt");
+            try(FileReader fileReader = new FileReader(file)) {
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                String line = null;
 
-        try(FileInputStream fileInputStream = new FileInputStream(file)) {
-            System.out.println("Total size to read in bytes is " + fileInputStream.available());
+                while((line = bufferedReader.readLine()) != null){
+                    System.out.println(line);
+                }
 
-            int content ;
-            while((content = fileInputStream.read()) != -1) {
-                //convert to char and display it
-                System.out.print((char) content);
+            } catch (Exception e){
+
             }
-        } catch (Exception e){
 
-        }
 
-//        byte data[] = new byte[12];
+
+
+
+
+
+
+
+//        try(FileInputStream fileInputStream = new FileInputStream(file)) {
+//
+//            System.out.println("Total Size to read in bytes is " + fileInputStream.available());
+//            int content;
+//            while( (content = fileInputStream.read()) != -1) {
+//                System.out.print((char) content);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        byte data[] = new byte[11];
 //        System.out.println("Enter some characters : ");
 //        System.in.read(data);
 //        System.out.println("You entered : ");
 //
-//        for(int i = 0; i < data.length;i++){
-//            System.out.print((char) data[i]);
+//        for(int i = 0; i < data.length; i++){
+//            System.out.print((char)data[i]);
 //        }
     }
 }
